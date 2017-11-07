ab43d0709bcf1bcdeaefef656fea8f732bb14499
BlueRyno <ryhamman@gmail.com>
2017-11-07 14:04:22 -0800

    Create list.js

---
+++ app/list.js
@@ -0,0 +1,22 @@
+creatures = [
+  {
+    version: "5e",
+    source: [
+      {
+        name: "Volo's Guide",
+        author: "Wizards of the Coast",
+        content: {
+          
+        }
+      },
+      
+      {
+        name: "Monster Manual",
+        author: "Wizards of the Coast",
+        content: {
+
+        }
+      }
+    ]
+  }
+]
