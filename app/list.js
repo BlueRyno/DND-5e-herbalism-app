e03c29d41f18473555b037142836fc89a53fd355
BlueRyno <ryhamman@gmail.com>
2017-11-08 07:24:23 -0800

    Working on the basic functionality. Building the creatue list.

--- app/list.js
+++ app/list.js
@@ -1,22 +1,62 @@
-creatures = [
-  {
-    version: "5e",
-    source: [
+creatures = {
+  playersHandbook: {
+    author: "Wizards of the Coast",
+    content: [
       {
-        name: "Volo's Guide",
-        author: "Wizards of the Coast",
-        content: {
-          
-        }
-      },
-      
-      {
-        name: "Monster Manual",
-        author: "Wizards of the Coast",
-        content: {
 
-        }
+      }
+    ]
+  },
+  
+  monsterManual: {
+    author: "Wizards of the Coast",
+    content: {
+      aarakocra: {
+        xp: 50,
+
+        type: "humanoid",
+        family: "aarakocra",
+        monster: false,
+
+        environment: [
+          "air",
+          "plains",
+          "hills",
+          "mountains"
+        ]
+      },
+
+      aboleth: {
+        xp: 5900,
+        
+        type: "aberration",
+        monster: true,
+        
+        environment: [
+          "aquatic"
+        ]
+      },
+
+      deva: {
+        xp: 5900,
+
+        type: "celestial",
+        family: "angel",
+        monster: false,
+
+        environment: [
+          "any"
+        ]
+      }
+    }
+  },
+
+  volosGuide: {
+    author: "Wizards of the Coast",
+    content: [
+      {
+        
       }
     ]
   }
-]
+}
